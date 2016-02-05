75188b9fc421a9135cbf59449c60e63dc4e2ca0e
dariano <darianosoares@hotmail.com>
2016-02-05 01:36:52 -0200

    Implementado o model da urna e adicionado modulos para testes.

--- app/models/urnas/schema-urna.js
+++ app/models/urnas/schema-urna.js
@@ -2,7 +2,9 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const _schema = {
-	votos: require('./votoModel'),
+	votos: [ require('./schema-voto')],
+	localidade: { type: String , required: true },
+	semanaDaVotacao: { type: Number, required: true},
 	created_at: require('./fields/fields-created-at')
 };
 
---
+++ app/models/urnas/urnaModel.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const Schema = require('./schema-urna');
+const Urna = mongoose.model('urna', Schema);
+
+'use strict';
+
+const votar = frequire('./../actions/action-create')(Urna);
+
+const actions = {
+	votar
+};
+
+module.exports = actions;
